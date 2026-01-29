8b37c7e33dcc4ed059df3fbcb38a1ce8161439a1
Carlos <carlosaac232001@gmail.com>
2026-01-29 10:50:52 -0500

    fix: add more options to create a suitable created date string

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -17,6 +17,9 @@ export function postNewMessage(req, res) {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
   };
 
   messages.push({
