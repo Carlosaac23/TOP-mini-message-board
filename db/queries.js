c006d3e6e84e9d95ebee6682c357db54a17dadcd
Carlos <carlosaac232001@gmail.com>
2026-02-01 15:46:49 -0500

    feat: create main functions to get all, get one and add a message from db

---
+++ db/queries.js
@@ -0,0 +1,21 @@
+import { pool } from './pool.js';
+
+export async function getAllMessages() {
+  const { rows } = await pool.query('SELECT * FROM messages');
+  console.log('Rows: ', rows);
+  return rows;
+}
+
+export async function addMessage(name, message) {
+  await pool.query('INSERT INTO messages (name, message) VALUES ($1, $2)', [
+    name,
+    message,
+  ]);
+}
+
+export async function getMessage(messageID) {
+  const { rows } = await pool.query('SELECT * FROM messages WHERE id = ($1)', [
+    messageID,
+  ]);
+  return rows[0];
+}
