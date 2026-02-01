ae4dba08d32e58c4b7de8781d08d68255b675e1a
Carlos <carlosaac232001@gmail.com>
2026-02-01 11:31:23 -0500

    feat: create db pool and connect to Railway DB

---
+++ db/pool.js
@@ -0,0 +1,8 @@
+import { configDotenv } from 'dotenv';
+configDotenv();
+
+import { Pool } from 'pg';
+
+export const pool = new Pool({
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+});
