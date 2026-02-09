54dd6f9220014dea7d085c24ff9ed0a2110c7ec0
Carlos <carlosaac232001@gmail.com>
2026-02-09 12:32:14 -0500

    feat: use new sanitizer functions to prevent db crash

--- db/queries.js
+++ db/queries.js
@@ -1,24 +1,34 @@
 import { pool } from './pool.js';
+import { safeQuerySingle, safeQueryMany } from './safeQueries.js';
 
 export async function getAllMessages() {
-  const { rows } = await pool.query('SELECT * FROM messages');
-  return rows;
+  return await safeQueryMany(
+    () => pool.query('SELECT * FROM messages'),
+    'getAllMessage()'
+  );
 }
 
 export async function addMessage(name, message) {
-  await pool.query('INSERT INTO messages (name, message) VALUES ($1, $2)', [
-    name,
-    message,
-  ]);
+  return await safeQuerySingle(
+    () =>
+      pool.query('INSERT INTO messages (name, message) VALUES ($1, $2)', [
+        name,
+        message,
+      ]),
+    `addMessage(${name}, ${message})`
+  );
 }
 
 export async function getMessage(messageID) {
-  const { rows } = await pool.query('SELECT * FROM messages WHERE id = ($1)', [
-    messageID,
-  ]);
-  return rows[0];
+  return await safeQuerySingle(
+    () => pool.query('SELECT * FROM messages WHERE id = $1', [messageID]),
+    `getMessage(${messageID})`
+  );
 }
 
 export async function deleteMessage(messageID) {
-  await pool.query('DELETE FROM messages WHERE id = ($1)', [messageID]);
+  return await safeQuerySingle(
+    () => pool.query('DELETE FROM messages WHERE id = $1', [messageID]),
+    `deleteMessage(${messageID})`
+  );
 }
